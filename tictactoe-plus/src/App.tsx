7d575b92dec259a9a8cd84240ab0c467378e2756
estratechs <38571570+estratechs@users.noreply.github.com>
2025-10-19 18:05:12 -0600

    update

--- tictactoe-plus/src/App.tsx
+++ tictactoe-plus/src/App.tsx
@@ -41,7 +41,6 @@ export function SettingsFooter(){
 export {};
 // --- End enhancements ---
 
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
